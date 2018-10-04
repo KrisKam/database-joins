aad7ab4d62a5e1bf40c53730469513181fbe96fe
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-04 17:10:48 -0600

    Add reviews table and seed file

---
+++ migrations/20181004164230_reviews.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("reviews", (review) => {
+    review.increments("reviewID").primary();
+    review.foreign("bookID").references("books.id");
+    review.string("quote", 400);
+    review.string("source");
+    review.boolean("positive");
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("reviews");
+};
