d4502d8a9bcce6afd51c6ca3b96625211783a381
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-04 18:10:17 -0600

    Corrected foreign key relationship in reviews schema

--- seeds/01_reviews.js
+++ seeds/01_reviews.js
@@ -4,14 +4,14 @@ exports.seed = function(knex, Promise) {
   return knex("reviews").del()
     .then(function () {
       // Inserts seed entries
-      return knex(reviews).insert([
-        {bookID: 1, quote: "Glorious.", source: "New Yorker", positive: true},
-        {bookID: 1, quote: "Blissfully Romantic….A strange, terrific, spellcasting story.", source: "San Francisco Chronicle", positive: true},
-        {bookID: 2, quote: "So serenely beautiful and written in a prose so gravely measured and thoughtful, that one feels touched with grace just to read it.", source: "Washington Post", positive: true},
-        {bookID: 2, quote: "Lyrical and meditative . . . potently contemplative.", source: "Time", positive: true},
-        {bookID: 3, quote: "Remarkable . . . an even stronger accomplishment than Gilead.", source: "New York Review of Books", positive: true},
-        {bookID: 4, quote: "An ambitious work on so many levels at once.", source: "Chicago Tribune", positive: true},
-        {bookID: 5, quote: "A triumph―a genuinely new story, a genuinely new form.", source: "New Yorker", positive: true}
+      return knex("reviews").insert([
+        {books_id: 1, quote: "Glorious.", source: "New Yorker", positive: true},
+        {books_id: 1, quote: "Blissfully Romantic….A strange, terrific, spellcasting story.", source: "San Francisco Chronicle", positive: true},
+        {books_id: 2, quote: "So serenely beautiful and written in a prose so gravely measured and thoughtful, that one feels touched with grace just to read it.", source: "Washington Post", positive: true},
+        {books_id: 2, quote: "Lyrical and meditative . . . potently contemplative.", source: "Time", positive: true},
+        {books_id: 3, quote: "Remarkable . . . an even stronger accomplishment than Gilead.", source: "New York Review of Books", positive: true},
+        {books_id: 4, quote: "An ambitious work on so many levels at once.", source: "Chicago Tribune", positive: true},
+        {books_id: 5, quote: "A triumph―a genuinely new story, a genuinely new form.", source: "New Yorker", positive: true}
       ]);
     });
 };
